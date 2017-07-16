5a0f6cd3b36aa91fe259c783707c7056517d4e84
chaadel <keran19@naver.com>
2017-07-16 16:12:49 +0900

    .

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// var weaponList = ["AK-47", "Gun", "Machete", "knife"];
+// var countries = ["korea", "Japan", "Greece", "Colombia"]
+
+// var soldier = {
+  // name: "cha",
+  // weapon: weaponList[1],
+  // country: countries[3],
+  // isDead: false,
+  // shoot: function(){
+    // console.log("Pew Pew Pew");
+  // },
+  // sayHi: function(sargent){
+    // console.log("Hello" + sargent + "My name is" + this.name)
+  // }
+// }
+
+// function multiply(chicken, potato) {
+  // return chicken * potato;
+// }
+// function division(lala, lolo) {
+  // return lala / lolo;
+// }
+// var age = prompt("How old are u?");
+//
+// if (age < 18) {
+//   console.log("Get out!");
+// }
+// else if(age > 18 && age < 45){
+//   console.log("You are an adult.")
+// }
+// else {
+//   console.log("Cheers!");
+// }
+
+$(document).ready(function(){
+  var $heart = $(".heart");
+  $heart.click(function() {
+    $(this).toggleClass("fa-heart-o");
+    $(this).toggleClass("fa-heart heart-click");
+  });
+  var $comment = $(".comment");
+  $comment.click(function() {
+  $(this).toggleClass("fa-comment-o");
+  $(this).toggleClass("fa-comment");
+});
+});
